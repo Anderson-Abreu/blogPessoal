09a2a9a91400e691048215785a7b1ae4a4c15c2d
Anderson-Abreu <prof.andersonmabreu@gmail.com>
2020-10-09 13:56:32 -0300

    atualização do projeto blogPessoal até post de temas e postagens

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { PostagemService } from '../servece/postagem.service';
+import { TemaService } from '../servece/tema.service';
+import {Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +13,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  key='data'
+  reverse= true
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+  titulo: string
 
-  ngOnInit(): void {
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+  nomeTema: string
+
+  constructor(
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+    this.findAllPostagens()
+    this.findAllTemas()
   }
 
+  findAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp:Postagem[])=>{
+      this.listaPostagens=resp
+    })
+
+  }
+
+  publicar(){
+    this.tema.id= this.idTema
+    this.postagem.tema=this.tema
+
+    if(this.postagem.titulo==null||this.postagem.texto==null||this.postagem.tema==null){
+      alert('Preencha todos os campos antes de publicar!!!')
+    }else{
+      this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+        this.postagem=resp
+        this.postagem=new Postagem()
+        alert('Postagem realizada com sucesso!!!')
+        this.findAllPostagens()
+      })
+    }
+  }
+
+  findAllTemas(){
+    this.temaService.getAllTemas().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp;
+    })
+  }
+  
 }
